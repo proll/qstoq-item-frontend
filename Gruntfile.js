a059a484b958dd56c5ce17e90b898ea5d5974205
proll <gleb.proll@gmail.com>
2014-01-06 18:15:07 +0400

    buildfix

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function(grunt) {
 			dist: {
 				options: {
 					collapseBooleanAttributes: true,
-					removeAttributeQuotes: true,
+					// removeAttributeQuotes: true,
 					removeRedundantAttributes: true,
 					removeEmptyAttributes: true,
 				},
@@ -261,7 +261,6 @@ module.exports = function(grunt) {
 		'copy:dist',
 		'rev',
 		'usemin',
-		// 'htmlmin:dist',
 		'htmlmin:deploy',
 	]);
 };
